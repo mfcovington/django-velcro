f42b3f3dcdda2dd1ce20eb6ee0ebc3894c822ccc
Mike Covington <mfcovington@gmail.com>
2015-09-22 15:01:50 -0700

    Rename 'object'/'object_type' to 'related_object'/'related_type' for 'velcro_url' template tag

--- django_velcro/templatetags/velcro_tags.py
+++ django_velcro/templatetags/velcro_tags.py
@@ -14,22 +14,24 @@ def find_dict_in_list(list_, key, value):
     return []
 
 @register.simple_tag(takes_context=True)
-def velcro_url(context, object=None, object_type=None):
+def velcro_url(context, related_object=None, related_type=None):
     """
     Template tag to get the reverse URL for a related object.
-    Only requires arguments if 'object' and/or 'object_type' are not defined.
+    Only requires arguments if 'related_object' and/or 'related_type'
+    are not defined.
     """
-    if object == None:
-        object = context['object']
-    if object_type == None:
-        object_type = context['object_type']
+    if related_object is None:
+        related_object = context['related_object']
+    if related_type is None:
+        related_type = context['related_type']
 
-    object_type_metadata = VELCRO_METADATA[object_type]
-    model_metadata = find_dict_in_list(object_type_metadata, 'model',
-        object.__class__.__name__)
-    view = model_metadata['view']
-    url_args = model_metadata['url_args']
-    return reverse(view, args=[getattr(object, arg) for arg in url_args])
+    related_type_metadata = VELCRO_METADATA[related_type]
+    related_model_metadata = find_dict_in_list(
+        related_type_metadata, 'model', related_object.__class__.__name__)
+    view = related_model_metadata['view']
+    url_args = related_model_metadata['url_args']
+    return reverse(
+        view, args=[getattr(related_object, arg) for arg in url_args])
 
 @register.inclusion_tag('django_velcro/related_content.html')
 def velcro_related(object):
